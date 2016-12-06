b284c99e0a077a5d071276175b8105c224857f27
Swolebrain <thecodingteacher@gmail.com>
2016-12-05 21:36:21 -0500

    feat: finished crud api

--- Games.js
+++ Games.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/games-store');
 
 const GameSchema = new mongoose.Schema({
@@ -24,6 +25,40 @@ function getAllGames(req, res){
   .catch(err=>res.sendStatus(500));
 }
 
+function createNewGame(req, res){
+  console.log(req.body);
+  var g = new Game(req.body);
+  g.save()
+  .then(result=>res.json(result))
+  .catch(function(err){
+    console.log(err.toString());
+    res.sendStatus(500);
+  });
+}
+
+function updateGame(req, res){
+  var gameId = req.params.id;
+  Game.findByIdAndUpdate(gameId, req.body)
+  .then(result=>res.json(result))
+  .catch(err=>{
+    console.log(err.toString());
+    res.sendStatus(500);
+  });
+}
+
+function deleteGame(req, res){
+  var gameId = req.params.id;
+  Game.findByIdAndRemove(gameId)
+  .then(result=>res.json(result))
+  .catch(err=>{
+    console.log(err.toString());
+    res.sendStatus(500);
+  });
+}
+
 module.exports = {
-  getAll: getAllGames
+  getAll: getAllGames,
+  newGame: createNewGame,
+  updateGame: updateGame,
+  delete: deleteGame
 };
